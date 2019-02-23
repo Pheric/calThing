0c21ac432a6d1f97b980b1927e203cc5ccd4d360
Tristan Fletcher <tristanfletcher52@gmail.com>
2019-02-22 19:46:39 -0600

    Added database init script

---
+++ initDatabase.sql
@@ -0,0 +1,26 @@
+-- create and select the database
+DROP DATABASE IF EXISTS activitiesPost;
+CREATE DATABASE activitiesPost;
+USE activitiesPost;  -- MySQL command
+
+-- create the tables
+CREATE TABLE events (
+    eventID INT NOT NULL PRIMARY KEY,
+    eventName VARCHAR(255) NOT NULL,
+    eventLocation VARCHAR(255) NOT NULL,
+    eventTime VARCHAR(255) NOT NULL
+);
+
+INSERT INTO events VALUES
+(1, "testing", "BIT235", "midnight to sunrise");
+
+-- create the users and grant priveleges to those users
+GRANT SELECT, INSERT, DELETE, UPDATE
+ON activitiesPost.*
+TO dbUser@localhost
+IDENTIFIED BY 'Password1!';
+
+GRANT SELECT
+ON events
+TO dbTester@localhost
+IDENTIFIED BY 'Password1!';
