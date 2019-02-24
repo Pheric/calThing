285515906fcc1d632a92f2ec9dbcc34240eb7f9d
Eric Graham <photon0f@protonmail.com>
2019-02-23 20:30:36 -0600

    Modify initDb file

--- initDatabase.sql
+++ initDatabase.sql
@@ -1,34 +1,34 @@
--- create and select the database
 DROP DATABASE IF EXISTS activitiesPost;
 CREATE DATABASE activitiesPost;
-USE activitiesPost;  -- MySQL command
+USE activitiesPost;
 
 CREATE TABLE categories (
     categoryId INT AUTO_INCREMENT,
-    categoryName TEXT NOT NULL UNIQUE,
+    categoryName VARCHAR(30) NOT NULL UNIQUE,
     CONSTRAINT category_pk PRIMARY KEY (categoryId)
 );
 
 CREATE TABLE events (
     eventId INT AUTO_INCREMENT PRIMARY KEY,
     categoryId INT NOT NULL,
-    eventName TEXT NOT NULL,
-    eventDescription TEXT NOT NULL,
-    eventLocation TEXT NOT NULL,
-    eventTime TEXT NOT NULL
+    eventName VARCHAR(128) NOT NULL,
+    eventDescription VARCHAR(6198) NOT NULL,
+    eventLocation VARCHAR(256) NOT NULL,
+    eventTime VARCHAR(128) NOT NULL,
+    postDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO categories VALUES
-(1, "Athletics"),
-(2, "Careers"),
-(3, "Academic"),
-(4, "Clubs"),
-(5, "Announcements");
+INSERT INTO categories (categoryName) VALUES
+("Athletics"),
+("Careers"),
+("Academic"),
+("Clubs"),
+("Announcements");
 
-INSERT INTO events VALUES
-(1, 1, "testing1", "making sure the database works1", "BIT235-1", "midnight to sunrise1"),
-(2, 1, "testing2", "making sure the database works2", "BIT235-2", "midnight to sunrise2"),
-(3, 2, "testing3", "making sure the database works3", "BIT235-3", "midnight to sunrise3"),
-(4, 3, "testing4", "making sure the database works4", "BIT235-4", "midnight to sunrise4"),
-(5, 4, "testing5", "making sure the database works5", "BIT235-5", "midnight to sunrise5"),
-(6, 5, "testing6", "making sure the database works6", "BIT235-6", "midnight to sunrise6");
+INSERT INTO events (categoryId, eventName, eventDescription, eventLocation, eventTime) VALUES
+(1, "testing1", "making sure the database works1", "BIT235-1", "midnight to sunrise1"),
+(1, "testing2", "making sure the database works2", "BIT235-2", "midnight to sunrise2"),
+(2, "testing3", "making sure the database works3", "BIT235-3", "midnight to sunrise3"),
+(3, "testing4", "making sure the database works4", "BIT235-4", "midnight to sunrise4"),
+(4, "testing5", "making sure the database works5", "BIT235-5", "midnight to sunrise5"),
+(5, "testing6", "making sure the database works6", "BIT235-6", "midnight to sunrise6");
